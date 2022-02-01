fc5f758742865af6ca518684cbb4ef8d837857f0
Magda <magda@MacBook-Pro-Magda.local>
2022-02-01 13:28:43 +0100

    refactore code

--- game.js
+++ game.js
@@ -1,12 +1,20 @@
-const gameSpace = document.getElementsByClassName('main__container')[0]
+const gameSpace = document.getElementsByClassName('main__container')
+const [container] = gameSpace
 
-const gameCards = Array.from(Array(20))
-gameCards.forEach(() => {
+Array
+.from(Array(20))
+.forEach(() => {
     const newCard = document.createElement('div')
     newCard.classList.add('card')
     newCard.addEventListener('click', () => {
+        if(newCard.classList.contains('flipCard')){
+        newCard.classList.remove('flipCard')
+        newCard.classList.add('flipCardBack')
+        } else {
         newCard.classList.add('flipCard')
+        newCard.classList.remove('flipCardBack')
+        }
     })
     
-    gameSpace.appendChild(newCard)
+    container.appendChild(newCard)
 })
