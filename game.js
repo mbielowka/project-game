6d2433a1850fa7242c3e0c045ba8e95aebad862f
Magda <magda@MacBook-Pro-Magda.local>
2022-02-01 14:58:53 +0100

    rename class in html, modify code in js

--- game.js
+++ game.js
@@ -1,20 +1,19 @@
-const gameSpace = document.getElementsByClassName('main__container')
-const [container] = gameSpace
+const [gameSpace] = document.getElementsByClassName('main__container')
 
 Array
-.from(Array(20))
-.forEach(() => {
-    const newCard = document.createElement('div')
-    newCard.classList.add('card')
-    newCard.addEventListener('click', () => {
-        if(newCard.classList.contains('flipCard')){
-        newCard.classList.remove('flipCard')
-        newCard.classList.add('flipCardBack')
-        } else {
-        newCard.classList.add('flipCard')
-        newCard.classList.remove('flipCardBack')
-        }
+    .from(Array(20))
+    .forEach(() => {
+        const newCard = document.createElement('div')
+        newCard.classList.add('card')
+        newCard.addEventListener('click', () => {
+            if (newCard.classList.contains('flipCard')) {
+                newCard.classList.remove('flipCard')
+                newCard.classList.add('flipCardBack')
+            } else {
+                newCard.classList.add('flipCard')
+                newCard.classList.remove('flipCardBack')
+            }
+        })
+        
+        gameSpace.appendChild(newCard)
     })
-    
-    container.appendChild(newCard)
-})
